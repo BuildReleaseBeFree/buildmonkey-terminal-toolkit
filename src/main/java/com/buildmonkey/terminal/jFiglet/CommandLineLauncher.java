9a87952b00dfffaec5a39b8b02237eaf66bda3df
James Borkowski <v-jborkowski@expedia.com>
2014-03-24 18:27:43 +0000

    James - tidy up

--- src/main/java/com/buildmonkey/terminal/jFiglet/CommandLineLauncher.java
+++ src/main/java/com/buildmonkey/terminal/jFiglet/CommandLineLauncher.java
@@ -7,12 +7,23 @@ public class CommandLineLauncher {
 
     //Command line argument: -addNumbers adds all the number arguments after it
     public static void main(String[] args) throws Exception {
+        String usage = "Usage:\n" +
+                "\n" +
+                "   -l (or) --list                         List all fonts available\n" +
+                "   -a (or) --display-all                  Display ALL fonts with sample\n" +
+                "   -s (or) --sample-font [FONT]           Sample particular [FONT]\n" +
+                "   -S (or) --sample-font-as [FONT] [TEXT] Sample [FONT] with [TEXT]\n" +
+                "   -t (or) --sample-text [TEXT]           Sample [TEXT] with all fonts\n";
 
         //Check to see if the String array is empty
         //Display a message to the user if it is
         if (args.length == 0)
         {
-            System.out.println("No command-line arguments have been entered!");
+            System.out.println("\nNo command-line arguments have been given!");
+            // Our header
+            System.out.println(FigletFont.convertOneLineAsFont("fonts/rectangles" , "BuildMonkey Term Tools"));
+            // Show usage screen...
+            System.out.println(usage);
         }
         else
         {
@@ -29,57 +40,79 @@ public class CommandLineLauncher {
                 }
             }
 
-            //boolean validNumbers = false;
-            boolean addNumbers = false;
-            //double total = 0;
-
             //Iterate through each string in the String array.
-            for(String argument: args) {
+            //REMEMBER!: Any arguments before the matching one will be ignored.
+            for(int i = 0; i < args.length; i++) {
+                String argument = args[i];
+
+                //Look for the -h or --help or -? parameter
+                if (argument.equals("-h") || argument.equals("--help") || argument.equals("-?")) {
+                    // Our header
+                    System.out.println(FigletFont.convertOneLineAsFont("fonts/rectangles" , "BuildMonkey Term Tools"));
+                    // Show usage screen...
+                    System.out.println(usage);
+                }
+
                 //Look for the -l or --list parameter
-                //Any arguments before this will be ignored.
-                if (argument.equals("-l") || argument.equals("--list")) {
-                    // List all fonts
-                    for (String name : fonts) {
-                        String filenameToRender = name.replace(".", "/");
-                        System.out.println(name);
-                        // System.out.println(convertOneLineAsFont(filenameToRender , text));
-                        // System.out.println(FigletFont.convertOneLineAsFont(filenameToRender , "abcdefghijklmnopqrstuvwxyz"));
+                else if (argument.equals("-l") || argument.equals("--list")) {
+                    // Our header
+                    System.out.println(FigletFont.convertOneLineAsFont("fonts/rectangles" , "BuildMonkey Term Tools"));
+                    System.out.println("Fonts Available for Selection:\n");
+                    // Lets remove the 'font.' from the start that we get as its in a fonts directory
+                    for(int j=0; j<fonts.length;j++) {
+                        fonts[j] = fonts[j].toString().replaceFirst("fonts.", "");
                     }
+
+                    // List all fonts in a 4 column table
+                    for (int leftIndex=0; leftIndex<(fonts.length / 4); ++leftIndex)
+                    {
+                        System.out.printf("%-24s %-24s %-24s %-24s\n", fonts[leftIndex],
+                                fonts[leftIndex + (fonts.length / 4)], fonts[leftIndex + ((fonts.length / 4) * 2)], fonts[leftIndex + ((fonts.length / 4) * 3)]);
+                    }
+                    System.out.println("\nCurrently " + fonts.length + " fonts available to select from...\n"   );
+
                 }
 
                 //Look for the -a or --display-all parameter
-                //Any arguments before this will be ignored.
                 else if (argument.equals("-a") || argument.equals("--display-all")) {
+                    System.out.println("\n");
                     // List all fonts
                     for (String name : fonts) {
                         String filenameToRender = name.replace(".", "/");
                         System.out.println(name);
-                        // System.out.println(convertOneLineAsFont(filenameToRender , text));
                         System.out.println(FigletFont.convertOneLineAsFont(filenameToRender, "abcdefghijklmnopqrstuvwxyz"));
                     }
                 }
 
+                //Look for the -s or --sample-font parameter
+                else if (argument.equals("-s") || argument.equals("--sample-font")) {
+                    String fontNameToSample = args[i+1];
+                    System.out.println("\n" + fontNameToSample + ":");
+                    System.out.println(FigletFont.convertOneLineAsFont("fonts/" + fontNameToSample, "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")+"\n");
+                }
 
-//                else if (addNumbers) //addNumbers will be true once the -addNumbers has been found
-//                {
-//                    try {
-//                        //Use the double class to be able to handle ints and doubles.
-//                        total = total + Double.parseDouble(argument);
-//                        validNumbers = true;
-//
-//                        //Catch any NumberFormatExceptions and display a message to the user
-//                    } catch (NumberFormatException e) {
-//                        System.out.println("The arguments you have entered are not all numbers.");
-//                        validNumbers = false;
-//                        addNumbers = false;
-//                    }
-//
-//                }
-//            }
-//            if (validNumbers) //will be false if one of the arguments is not a number
-//            {
-//                System.out.println("The total value of the arguments is: " + total);
-//            }
+                //Look for the -S or --sample-font-as parameter
+                else if (argument.equals("-S") || argument.equals("--sample-font-as")) {
+                    String fontNameToSample = args[i+1];
+                    String fontTextToSample = args[i+2];
+                    System.out.println("\n" + fontNameToSample + ":");
+                    System.out.println(FigletFont.convertOneLineAsFont("fonts/" + fontNameToSample, fontTextToSample)+"\n");
+                }
+
+                //Look for the -S or --sample-font-as parameter
+                else if (argument.equals("-t") || argument.equals("--sample-text")) {
+                    System.out.println("\n");
+                    // Lets remove the 'font.' from the start that we get as its in a fonts directory
+                    for(int j=0; j<fonts.length;j++) {
+                        fonts[j] = fonts[j].toString().replaceFirst("fonts.", "");
+                    }
+                    String fontTextToSample = args[i+1];
+                    for (String name : fonts) {
+                        String filenameToRender = name;
+                        System.out.println(name);
+                        System.out.println(FigletFont.convertOneLineAsFont("fonts/" + filenameToRender, fontTextToSample));
+                    }
+                }
             }
         }
     }
